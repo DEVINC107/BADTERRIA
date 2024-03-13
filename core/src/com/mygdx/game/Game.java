7db6298d5373a8650462c18e1bfba1e9db4f8b1d
BT_1N3_24 <a@a.com>
2024-03-13 11:07:14 -0400

    moving box

--- core/src/com/mygdx/game/Game.java
+++ core/src/com/mygdx/game/Game.java
@@ -1,18 +1,43 @@
 package com.mygdx.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.graphics.Camera;
+import com.badlogic.gdx.graphics.PerspectiveCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
 
 public class Game extends ApplicationAdapter {
+	World world;
 	SpriteBatch batch;
+	Box2DDebugRenderer debugRenderer;
 	Texture img;
+	Camera camera;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
+		world = new World(new Vector2(0, -10), true);
+		debugRenderer = new Box2DDebugRenderer();
+		camera = new PerspectiveCamera();
 		img = new Texture("badlogic.jpg");
+		// Create our body definition
+		BodyDef groundBodyDef = new BodyDef();
+// Set its world position
+		groundBodyDef.position.set(new Vector2(0, 2));
+
+// Create a body from the definition and add it to the world
+		Body groundBody = world.createBody(groundBodyDef);
+
+// Create a polygon shape
+		PolygonShape groundBox = new PolygonShape();
+// Set the polygon shape as a box which is twice the size of our view port and 20 high
+// (setAsBox takes half-width and half-height as arguments)
+		groundBox.setAsBox(camera.viewportWidth * 2, 2.0f);
+// Create a fixture from our polygon shape and add it to our ground body
+		groundBody.createFixture(groundBox, 0.0f);
+// Clean up after ourselves
+		//groundBox.dispose();
 	}
 
 	@Override
@@ -21,6 +46,8 @@ public class Game extends ApplicationAdapter {
 		batch.begin();
 		batch.draw(img, 0, 0);
 		batch.end();
+		world.step(1/60f, 6, 2);
+		debugRenderer.render(world, camera.combined);
 	}
 	
 	@Override
